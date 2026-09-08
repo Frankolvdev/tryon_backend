e77f20d5eb1ab60d5f222e28cdd6fc0949e10046
frank Olvera <frank.olv.dev@gmail.com>
2026-09-07 22:10:47 -0600

    Fix Models IA position migration revision id

--- alembic/versions/071_model_generation_asset_position.py
+++ alembic/versions/071_model_generation_asset_position.py
@@ -1,13 +1,13 @@
 """add position to model generation assets
 
-Revision ID: 071_model_generation_asset_position
+Revision ID: 071_model_asset_position
 Revises: 070_model_generation_assets
 Create Date: 2026-09-07
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = "071_model_generation_asset_position"
+revision = "071_model_asset_position"
 down_revision = "070_model_generation_assets"
 branch_labels = None
 depends_on = None
